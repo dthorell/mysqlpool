87f16c1b8cd0642e8af7e712c9b376d399241e3a
Daniel Thorell <dthorell@outlook.com>
2019-04-05 17:34:02 +0200

    Initial commit

---
+++ flask_mysqlpool/__init__.py
@@ -0,0 +1,51 @@
+"""
+    flask_mysqlpool
+    ~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2019 by Daniel Thorell.
+    :license: BSD, see LICENSE for more details.
+"""
+
+import mysql.connector.pooling
+from flask import current_app, _app_ctx_stack
+
+__version__ = '1.0.1'
+
+
+class MySQLPool(object):
+    def __init__(self, app=None):
+        self.cnx = None
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        app.config.setdefault('MYSQL_HOST', 'localhost')
+        app.config.setdefault('MYSQL_PORT', 3306)
+        app.config.setdefault('MYSQL_USER', 'root')
+        app.config.setdefault('MYSQL_PASS', '')
+        app.config.setdefault('MYSQL_DB', 'tmp')
+        app.config.setdefault('POOL_NAME', 'mysql_pool')
+        app.config.setdefault('POOL_SIZE', 5)
+        app.config.setdefault('AUTOCOMMIT', True)
+
+    def connect(self):
+        connection = {
+            'pool_name': current_app.config['POOL_NAME'],
+            'pool_size': current_app.config['POOL_SIZE'],
+            'autocommit': current_app.config['AUTOCOMMIT'],
+            'host': current_app.config['MYSQL_HOST'],
+            'port': current_app.config['MYSQL_PORT'],
+            'user': current_app.config['MYSQL_USER'],
+            'password': current_app.config['MYSQL_PASS'],
+            'database': current_app.config['MYSQL_DB'],
+        }
+        self.cnx = mysql.connector.pooling.MySQLConnectionPool(**connection)
+        return self.cnx
+
+    @property
+    def connection(self):
+        ctx = _app_ctx_stack
+        if not hasattr(ctx, 'mysqlpool'):
+            ctx.mysqlpool = self.connect()
+        return ctx.mysqlpool
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask_mysqlpool",
+    version="1.0.1",
+    author="Daniel Thorell",
+    author_email="dthorell@outlook.com",
+    description="Wrapper for mysql.connector.python to get a pool of mysql connections",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dthorell/mysqlpool",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+        'flask >= 1.0.0',
+        'mysql-connector-python >= 8.0.0'
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        "Operating System :: OS Independent",
+    ],
+)
